aa7bac38c189d2c3bc8f299b8b9e0d84c3e5d985
Ivo Pesak <hapesak@seznam.cz>
2022-11-01 16:55:21 +0100

    u

--- ParticlesTrackig/Program.cs
+++ ParticlesTrackig/Program.cs
@@ -2,3 +2,5 @@
 Directory.CreateDirectory("../../../output");
 var tracker = new CellTracker("../../../unet-pred-binarize", "../../../output");
 //var tracker = new CellTracker("../../../Test");
+
+tracker.TrackCells(true);
